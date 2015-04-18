5906686e20da80e00a45b18b86bd5788c3ec5725
JPen3 <jerry_pendleton@yahoo.com>
2015-04-18 17:47:05 -0500

    Beyond the bounds
    Stuff

--- GameJam15/Assets/Scripts/scrSpawnObj.cs
+++ GameJam15/Assets/Scripts/scrSpawnObj.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class scrSpawnObj : MonoBehaviour {
 
-	public GameObject boundary;
+	public GameObject[] boundary;
 	public float spawnTime = 3f;
 	public Transform[] spawnPoints;
 
@@ -20,9 +20,13 @@ public class scrSpawnObj : MonoBehaviour {
 	void Spawn ()
 	{
 		// Find a random index between zero and one less than the number of spawn points.
-		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		int spawnNum = Random.Range (1, 3);
 		
 		// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-		Instantiate (boundary, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		for (int i = 0; i < spawnNum; i++) {
+			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+			int boundaryIndex = Random.Range (0, boundary.Length);
+			Instantiate (boundary [boundaryIndex], spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+		}
 	}
 }
